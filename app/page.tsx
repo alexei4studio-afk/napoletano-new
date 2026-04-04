7a72fb2efe120c86a7990fbbb32982bbdd2e9f3e
alexei4studio-afk <alexei4studio@gmail.com>
2026-04-04 09:43:46 -0700

    Eliminare sectiune Instagram

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Hero from '@/components/Hero'
 import Menu from '@/components/Menu'
 import Story from '@/components/Story'
 import Gallery from '@/components/Gallery'
-import Events from '@/components/Events' // Importul nou pentru Evenimente
-import InstagramFeed from '@/components/InstagramFeed'
+import Events from '@/components/Events' 
 import Testimonials from '@/components/Testimonials'
 import Reservation from '@/components/Reservation'
 import Footer from '@/components/Footer'
@@ -18,8 +17,7 @@ export default function Home() {
       <Menu />
       <Story />
       <Gallery />
-      <Events /> {/* Secțiunea nouă: Capacitate 35 pers & Terasă */}
-      <InstagramFeed />
+      <Events /> {/* Secțiunea: Capacitate 35 pers & Terasă */}
       <Testimonials />
       <Reservation />
       <Footer />
